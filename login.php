4833aa3c5a1c2e305cd8808d1ba9086c111cc6cd
VKaysarov <v.i.kaisarov@gmail.com>
2021-03-29 10:36:05 +0500

    two commit

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     $password = $_POST['password'];
 
     if(empty($phone)) {
-        echo 'Телефон является обязательным для заполнения';
+        var_dump($_POST);
         exit();
     }
 
@@ -11,9 +11,12 @@
     $sql_query = 'SELECT * FROM `enrollee` WHERE phone="'.$phone.'" AND password="'.$password.'"';
     $result = $mysqli->query($sql_query);
     if ($result->num_rows == 1) {
-        header('Location: profile.html');
+        echo "Все ок";
+        // echo json_encode("Все ок");
+        // header('Location: profile.html');
     } else {
         echo 'Неверный логин или пароль';
+        // echo json_encode('Неверный логин или пароль');
     }
 
     // var_dump($result->fetch_row());
